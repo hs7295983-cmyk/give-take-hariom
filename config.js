def04d4823aafe45bd3061a07e8163b28aba8c33
Hariom Sharma <hs7295983@gmail.com>
2026-06-07 13:26:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.GIVE_TAKE_API_BASE = "https://give-take-beckend.onrender.com";
-window.GIVE_TAKE_SUPABASE_URL = "https://iwcbgztlrieawsvcxntg.supabase.co/rest/v1/";
+window.GIVE_TAKE_SUPABASE_URL = "https://iwcbgztlrieawsvcxntg.supabase.co";
 window.GIVE_TAKE_SUPABASE_ANON_KEY = "sb_publishable_-d26EgbTlUvZuvrYFTM-Wg_9WXRGSiH";
